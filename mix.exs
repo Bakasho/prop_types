1801a88daaffa89cb780ce3cfe6e29790401c160
John Faucett <jwaterfaucett@gmail.com>
2016-05-08 20:06:23 +0200

    add info

--- mix.exs
+++ mix.exs
@@ -5,13 +5,29 @@ defmodule PropTypes.Mixfile do
     [app: :prop_types,
      version: "0.0.1",
      elixir: "~> 1.2",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
+     description: description,
+     package: package,
      deps: deps]
   end
 
   def application do
     [applications: [:tipo]]
+  end
+
+  def description do
+    """
+    Property Type validations and checkers for elixir apps
+    """
+  end
+
+  def package do
+    [
+      name: :tipo,
+      files: ["lib", "mix.exs", "README.md", "LICENSE"],
+      licenses: ["MIT"],
+      maintainers: ["John Faucett", "Nathan Faucett"],
+      links: %{"GitHub" => "https://github.com/bakasho/prop_types"}
+    ]
   end
 
   defp deps do
